3a39fe4be8a7f346a1f8441b77fda28baf71fd69
Emil <mikayelyanemil19@gmail.com>
2025-02-08 18:40:07 +0400

    feat: add jwt token generation auth service

--- apps/api/src/auth/auth.service.ts
+++ apps/api/src/auth/auth.service.ts
@@ -2,15 +2,31 @@ import { Injectable } from '@nestjs/common';
 import { AuthTokens } from './interfaces/auth-tokens';
 import { CreateUserDto } from './dto/create-user.dto';
 import { ConfigService } from '@nestjs/config';
+import { JwtService } from '@nestjs/jwt';
+import { JWTPayload } from './interfaces/jwt-payload';
 
 @Injectable()
 export class AuthService {
     constructor(
-        private configService: ConfigService
+        private configService: ConfigService,
+        private jwtService: JwtService
     ) { }
 
     async signup(createUserDto: CreateUserDto): Promise<AuthTokens> {
-        const access_token = createUserDto.email, refresh_token = createUserDto.full_name;
+        return this.generateTokens({ sub: createUserDto.email, email: createUserDto.full_name });
+    }
+
+    private async generateTokens(payload: JWTPayload): Promise<AuthTokens> {
+        const [access_token, refresh_token] = await Promise.all([
+            this.jwtService.signAsync(payload, {
+                secret: this.configService.get<string>('JWT_ACCESS_SECRET'),
+                expiresIn: this.configService.get<string>('JWT_ACCESS_EXPIRY')
+            }),
+            this.jwtService.signAsync(payload, {
+                secret: this.configService.get<string>('JWT_REFRESH_SECRET'),
+                expiresIn: this.configService.get<string>('JWT_REFRESH_EXPIRY')
+            })
+        ]);
         return {
             access_token,
             refresh_token
