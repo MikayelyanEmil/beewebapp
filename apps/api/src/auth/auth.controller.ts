d7c6ca0689f47ee3610d1287307161195b4b912f
Emil <mikayelyanemil19@gmail.com>
2025-02-08 18:21:57 +0400

    feat: add signup controller route with client input validation

--- apps/api/src/auth/auth.controller.ts
+++ apps/api/src/auth/auth.controller.ts
@@ -1,13 +1,22 @@
-import { Controller, Post } from '@nestjs/common';
+import { Body, Controller, Post, Res, UsePipes, ValidationPipe } from '@nestjs/common';
 import { AuthService } from './auth.service';
+import { CreateUserDto } from './dto/create-user.dto';
+import { Response } from 'express';
+import { ConfigService } from '@nestjs/config';
 
 @Controller('auth')
 export class AuthController {
-    constructor(private authService: AuthService) {}
+    constructor(
+        private authService: AuthService,
+        private configService: ConfigService
+    ) { }
 
     @Post('signup')
-    async signup() {
-
+    @UsePipes(new ValidationPipe({ transform: true, whitelist: true })) // "cleans" incoming client data
+    async signup(@Body() createUserDto: CreateUserDto, @Res() res: Response) {
+        const { access_token, refresh_token } = await this.authService.signup(createUserDto);
+        res.cookie('refresh_token', refresh_token, { maxAge: this.configService.get('COOKIE_MAX_AGE'), httpOnly: true, sameSite: "none", secure: true });
+        res.status(200).json({ access_token });
     }
 
     @Post('login')
@@ -17,6 +26,6 @@ export class AuthController {
 
     @Post('logout')
     async logout() {
-        
+
     }
 }
--- apps/api/src/auth/auth.service.ts
+++ apps/api/src/auth/auth.service.ts
@@ -1,4 +1,19 @@
 import { Injectable } from '@nestjs/common';
+import { AuthTokens } from './interfaces/auth-tokens';
+import { CreateUserDto } from './dto/create-user.dto';
+import { ConfigService } from '@nestjs/config';
 
 @Injectable()
-export class AuthService {}
+export class AuthService {
+    constructor(
+        private configService: ConfigService
+    ) { }
+
+    async signup(createUserDto: CreateUserDto): Promise<AuthTokens> {
+        const access_token = createUserDto.email, refresh_token = createUserDto.full_name;
+        return {
+            access_token,
+            refresh_token
+        }
+    }
+}
