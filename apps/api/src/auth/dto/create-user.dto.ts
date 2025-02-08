95a892f53ff269aebd8b979a524597260cc878fb
Emil <mikayelyanemil19@gmail.com>
2025-02-08 17:22:05 +0400

    feat: add signup and login dtos with validation decorators

--- apps/api/src/auth/dto/create-user.dto.ts
+++ apps/api/src/auth/dto/create-user.dto.ts
@@ -1,10 +1,12 @@
+import { IsEmail, IsStrongPassword, MaxLength } from "class-validator";
+
 export class CreateUserDto {
-    // @MaxLength(30, { message: 'Please enter your valid name under 30 characters.' })
-    // full_name: string;
+    @MaxLength(30, { message: 'Please enter your valid name under 30 characters.' })
+    full_name: string;
 
-    // @IsEmail({}, { message: 'Please enter valid email address.' })
-    // email: string;
+    @IsEmail({}, { message: 'Please enter valid email address.' })
+    email: string;
 
-    // @IsStrongPassword({ minLength: 8, minNumbers: 4, minUppercase: 1, minSymbols: 1 }, { message: 'Password must be at least 8 characters long, contain 4 digits, 1 symbol, 1 uppercase.' })
-    // password: string;
+    @IsStrongPassword({ minLength: 8, minNumbers: 4, minUppercase: 1, minSymbols: 1 }, { message: 'Password must be at least 8 characters long, contain 4 digits, 1 symbol, 1 uppercase.' })
+    password: string;
 }
